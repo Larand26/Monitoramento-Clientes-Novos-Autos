e5d93cbe9778bc796727db1f2ac4bb396185d282
vbl09012003vbl@gmail.com <vbl09012003vbl@gmail.com>
2026-08-14 13:41:26 -0300

    feat: cria as funções de cron no index

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import appConfig from "./config/app.config.js";
+import cron from "node-cron";
+
+import { ingestNewCustomers, updateClients } from "./app.js";
+import { connectToMongoDB } from "./db/mongodb.js";
+
+(async () => {
+  try {
+    await connectToMongoDB();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+})();
+
+cron.schedule(appConfig.app.cronIngest, async () => {
+  console.log("Ingesting new customers...");
+  await ingestNewCustomers();
+});
+
+cron.schedule(appConfig.app.cronUpdate, async () => {
+  console.log("Updating clients...");
+  await updateClients();
+});
+
+if (appConfig.app.mode === "development") {
+  console.log("Running in development mode");
+  await ingestNewCustomers();
+  await updateClients();
+}
